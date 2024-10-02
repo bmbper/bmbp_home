4ca178166d212838ba0255334b21bf201835d083
zhangguokai <zhangguokai@inspur.com>
2024-10-02 22:19:31 +0800

    feat： 登录页面占位

--- web/static/js/login/bmbp/index.js
+++ web/static/js/login/bmbp/index.js
@@ -1,5 +1,24 @@
 // web/tsx/login/bmbp/index.tsx
 window.onload = () => {
   const root = ReactDOM.createRoot(document.getElementById("app"));
-  root.render(/* @__PURE__ */ React.createElement("div", null, "Login BMBP"));
+  root.render(/* @__PURE__ */ React.createElement(LoginForm, null));
+};
+var LoginForm = () => {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "login-container"
+  }, /* @__PURE__ */ React.createElement(arco.Form, null, /* @__PURE__ */ React.createElement(arco.Form.Item, {
+    label: "\u7528\u6237",
+    field: "username",
+    rules: [{ required: true }]
+  }, /* @__PURE__ */ React.createElement(arco.Input, {
+    placeholder: "\u8BF7\u8F93\u5165\u7528\u6237\u540D"
+  })), /* @__PURE__ */ React.createElement(arco.Form.Item, {
+    label: "\u5BC6\u7801",
+    field: "password",
+    rules: [{ required: true }]
+  }, /* @__PURE__ */ React.createElement(arco.Input, {
+    placeholder: "\u8BF7\u8F93\u5165\u5BC6\u7801"
+  })), /* @__PURE__ */ React.createElement(arco.Form.Item, null, /* @__PURE__ */ React.createElement(arco.Button, {
+    type: "primary"
+  }, "\u767B\u5F55"))));
 };
--- web/tsx/login/bmbp/index.tsx
+++ web/tsx/login/bmbp/index.tsx
@@ -1,4 +1,23 @@
 window.onload = () => {
     const root = ReactDOM.createRoot(document.getElementById("app"));
-    root.render(<div>LOGIN BMBP</div>);
+    root.render(<LoginForm/>);
 };
+const LoginForm = () => { 
+    return (
+        <div className="login-container">
+            <arco.Form>
+                <arco.Form.Item label='用户' field='username' rules={[{ required: true }]}>
+                    <arco.Input placeholder='请输入用户名' />
+                </arco.Form.Item>
+                <arco.Form.Item label='密码' field='password' rules={[{ required: true }]}>
+                    <arco.Input placeholder='请输入密码' />
+            </arco.Form.Item>
+            <arco.Form.Item>
+                <arco.Button type='primary'>
+                    登录
+                </arco.Button>
+            </arco.Form.Item>
+        </arco.Form>
+    </div>
+    )
+}
