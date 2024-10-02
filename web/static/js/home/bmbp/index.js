3fb6d0d7b899ddd2437729374db0a67c4f8ca228
zhangguokai <zhangguokai@inspur.com>
2024-10-02 22:48:19 +0800

    feat: home layout

--- web/static/js/home/bmbp/index.js
+++ web/static/js/home/bmbp/index.js
@@ -1,5 +1,18 @@
 // web/tsx/home/bmbp/index.tsx
 window.onload = () => {
   const root = ReactDOM.createRoot(document.getElementById("app"));
-  root.render(/* @__PURE__ */ React.createElement("div", null, "Home BMBP"));
+  root.render(/* @__PURE__ */ React.createElement(BmbpHomeLayout, null));
+};
+var BmbpHomeLayout = () => {
+  return /* @__PURE__ */ React.createElement("div", {
+    className: "bmbp-page"
+  }, /* @__PURE__ */ React.createElement(arco.Layout, {
+    style: { height: "100vh", width: "100vw" }
+  }, /* @__PURE__ */ React.createElement(arco.Layout.Header, {
+    style: { height: "48px", width: "100vw" }
+  }, "Header"), /* @__PURE__ */ React.createElement(arco.Layout, null, /* @__PURE__ */ React.createElement(arco.Layout.Sider, {
+    style: { width: "240px" }
+  }, "Sider"), /* @__PURE__ */ React.createElement(arco.Layout.Content, null, "Content")), /* @__PURE__ */ React.createElement(arco.Layout.Footer, {
+    style: { height: "24px" }
+  }, "Footer")));
 };
--- web/tsx/home/bmbp/index.tsx
+++ web/tsx/home/bmbp/index.tsx
@@ -1,4 +1,25 @@
 window.onload = () => {
-    const root = ReactDOM.createRoot(document.getElementById("app"));
-    root.render(<div>Home BMBP</div>);
+  const root = ReactDOM.createRoot(document.getElementById("app"));
+  root.render(<BmbpHomeLayout />);
+};
+
+const BmbpHomeLayout = () => {
+  return (
+    <div className="bmbp-page">
+      <arco.Layout style={{ height: "100vh", width: "100vw" }}>
+        <arco.Layout.Header style={{ height: "48px", width: "100vw" }}>
+          Header
+        </arco.Layout.Header>
+        <arco.Layout>
+          <arco.Layout.Sider style={{ width: "240px" }}>
+            Sider
+          </arco.Layout.Sider>
+          <arco.Layout.Content>Content</arco.Layout.Content>
+        </arco.Layout>
+        <arco.Layout.Footer style={{ height: "24px" }}>
+          Footer
+        </arco.Layout.Footer>
+      </arco.Layout>
+    </div>
+  );
 };
