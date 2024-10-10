3367784b9a332ffbd3ee28d7a85a15ade3745306
zhangguokai <zhangguokai@inspur.com>
2024-10-10 20:13:31 +0800

    feat: remove warn

--- src/view/vars.rs
+++ src/view/vars.rs
@@ -1,4 +1,3 @@
 pub const APP_ICON: &str = "systemAppIcon";
 pub const APP_TITLE: &str = "systemAppTitle";
-pub const APP_VERSION: &str = "systemAppVersion";
 pub const APP_COPY_RIGHT: &str = "systemAppCopyright";
--- src/view/view.rs
+++ src/view/view.rs
@@ -1,7 +1,7 @@
 use crate::ctx::ctx_init;
 use crate::init::CTX_TERA;
 use salvo::prelude::Text;
-use salvo::{handler, Depot, Request, Response};
+use salvo::{handler, Request, Response};
 
 use super::vars::{APP_COPY_RIGHT, APP_ICON, APP_TITLE};
 
@@ -16,7 +16,7 @@ pub async fn home_view(_: &mut Request, resp: &mut Response) {
 }
 
 #[handler]
-pub async fn welcome_view(_: &mut Request, depot: &mut Depot, resp: &mut Response) {
+pub async fn welcome_view(_: &mut Request, resp: &mut Response) {
     let ctx = ctx_init();
     let view_html = "home/welcome.html";
     resp.render(Text::Html((*CTX_TERA).render(view_html, &ctx).unwrap()));
@@ -30,7 +30,7 @@ pub async fn login_view(_: &mut Request, resp: &mut Response) {
 }
 
 #[handler]
-pub async fn root_view(_: &mut Request, depot: &mut Depot, resp: &mut Response) {
+pub async fn root_view(_: &mut Request, resp: &mut Response) {
     let ctx = ctx_init();
     let view_html = "login/bmbp.html";
     resp.render(Text::Html((*CTX_TERA).render(view_html, &ctx).unwrap()));
